3429f96549ee932d11cdfc7a008413c1fc7515a9
Oscar6 <oscar.m1024@gmail.com>
2019-02-08 16:04:53 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MemoryCard from "./MemoryCard.js";
+import "./App.css";
+
+function generateDeck() {
+  var symbols = ["∆", "ß", "£", "§", "•", "$", "+", "ø"];
+  var deck = [];
+  for (var i = 0; i < 16; i++) {
+    deck.push({
+      isFlipped: false,
+      symbol: symbols[i % 8],
+    });
+  }
+  shuffle(deck);
+  return deck;
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      deck: generateDeck(),
+      pickedCards: [],
+    };
+
+    this.pickCard = function pickCard(cardIndex) {
+      if (this.state.deck[cardIndex].isFlipped) {
+        return;
+      }
+      var cardToFlip = { ...this.state.deck[cardIndex] }
+      cardToFlip.isFlipped = true;
+
+      var newPickedCards = this.state.pickedCards.concat(cardIndex);
+      var newDeck = this.state.deck.map((card, index) => {
+        if (cardIndex == index) {
+          return cardToFlip;
+        }
+        return card;
+      });
+    }
+    this.setState({
+      deck: newDeck,
+      pickedCards: newPickedCards,
+    });
+  }
+
+
   render() {
+    var cardsJSX = this.state.deck.map((card, index) => {
+      return <MemoryCard symbol={card.symbol}
+        isFlipped={card.isFlipped}
+        key={index} />
+    });
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <a className="App-link">Memory Game</a>
+          <p className="subtitle">Match Cards to Win</p>
         </header>
+        <div>
+          {cardsJSX.slice(0, 4)}
+        </div>
+        <div>
+          {cardsJSX.slice(4, 8)}
+        </div>
+        <div>
+          {cardsJSX.slice(8, 12)}
+        </div>
+        <div>
+          {cardsJSX.slice(12, 16)}
+        </div>
       </div>
     );
   }
